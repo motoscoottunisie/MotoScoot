f950586f115f649b0fa0645aaba2bd782612560f
motoscoottunisie <motoscoottn@gmail.com>
2025-11-07 12:47:44 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
 import Home from './pages/Home';
 import SearchResults from './pages/SearchResults';
 import ListingDetail from './pages/ListingDetail';
@@ -10,17 +11,20 @@ import Dashboard from './pages/Dashboard';
 function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-gray-50">
+      <div className="min-h-screen bg-gray-50 flex flex-col">
         <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/search" element={<SearchResults />} />
-          <Route path="/listing/:id" element={<ListingDetail />} />
-          <Route path="/deposit" element={<DepositListing />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/favorites" element={<div className="p-8 text-center">Page Favoris - À développer</div>} />
-          <Route path="/messages" element={<div className="p-8 text-center">Page Messages - À développer</div>} />
-        </Routes>
+        <main className="flex-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/search" element={<SearchResults />} />
+            <Route path="/listing/:id" element={<ListingDetail />} />
+            <Route path="/deposit" element={<DepositListing />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/favorites" element={<div className="p-8 text-center">Page Favoris - À développer</div>} />
+            <Route path="/messages" element={<div className="p-8 text-center">Page Messages - À développer</div>} />
+          </Routes>
+        </main>
+        <Footer />
       </div>
     </Router>
   );
