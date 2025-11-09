721e5f05273956bd93e36b0e4d42005a492bcbfc
motoscoottunisie <motoscoottn@gmail.com>
2025-11-09 09:40:48 +0100

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -47,7 +47,7 @@ const Home: React.FC = () => {
         <div
           className="absolute inset-0"
           style={{
-            background: 'linear-gradient(180deg, #AF2E13 0%, #E65100 100%)',
+            background: 'linear-gradient(-180deg, #AF2E13 0%, #E65100 100%)',
             opacity: 0.95,
             mixBlendMode: 'multiply',
           }}
