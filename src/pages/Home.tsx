15208b0677b9205e75061b5cf1933af27ab1366b
motoscoottunisie <motoscoottn@gmail.com>
2025-11-09 08:13:44 +0100

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -56,7 +56,7 @@ const Home: React.FC = () => {
           </h1>
 
           {/* Glassmorphism Search Bar */}
-          <div className="max-w-xl mx-auto mb-8">
+          <div className="mx-auto mb-8" style={{ maxWidth: '42rem' }}>
             <form onSubmit={handleSearch} className="relative">
               <input
                 type="text"
